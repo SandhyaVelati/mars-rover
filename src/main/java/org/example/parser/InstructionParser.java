f3fa9dd86ba3ecf899284f4b63eee9d3d859e375
sandhya <svuksept2020@gmail.com>
2024-11-14 21:20:37 +0000

    feat: implement tests for InstructionParser

--- src/main/java/org/example/parser/InstructionParser.java
+++ src/main/java/org/example/parser/InstructionParser.java
@@ -2,16 +2,21 @@ package org.example.parser;
 
 
 
+import com.sun.source.tree.PatternTree;
 import org.example.model.InstructionEnum;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class InstructionParser {
     //LMLMMLLMMMR
     public List<InstructionEnum> parseInstructions(String instructions){
-        if(instructions.isBlank() || instructions.isEmpty()){
-            throw new RuntimeException("Instructions cannot be blank");
+        if(instructions==null || instructions.isBlank() || instructions.isEmpty()){
+            throw new IllegalArgumentException("Instructions cannot be blank");
+        }
+        if(!instructions.matches("[LRM]*")){
+            throw new IllegalArgumentException("Invalid Instruction found in the input, please pass L-to turn Left, R-to turn Right and M- to move a block");
         }
         List<InstructionEnum> ListOfInstructions = new ArrayList<>();
         for(char instruction: instructions.toCharArray()){
--- src/test/java/org/example/parser/InstructionParserTest.java
+++ src/test/java/org/example/parser/InstructionParserTest.java
@@ -1,12 +1,45 @@
 package org.example.parser;
 
+import org.example.model.InstructionEnum;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InstructionParserTest {
+    private final InstructionParser instructionParser = new InstructionParser();
+    @Test
+    @DisplayName("throws IllegalArgumentException, when any instruction other than LRM are passed as input")
+    void testInvalidInstructions() {
+        assertThrows(IllegalArgumentException.class,()->instructionParser.parseInstructions("LRMU"));
+    }
+    @Test
+    @DisplayName("throws IllegalArgumentException, when no instructions or empty instructions are passed")
+    void testEmptyInstruction() {
+        assertAll("check for empty or null instructions behaviour",
+                ()-> assertThrows(IllegalArgumentException.class,()->instructionParser.parseInstructions("")),
+                ()-> assertThrows(IllegalArgumentException.class,()->instructionParser.parseInstructions(null)),
+                ()-> assertThrows(IllegalArgumentException.class,()->instructionParser.parseInstructions("  ")));
+    }
+    @Test
+    @DisplayName("returns equal number of as List of InstructionEnum, when supplied valid number of arguments")
+    void testNumberOfInstructionsParsed() {
+        List<InstructionEnum> lmlmllrm = instructionParser.parseInstructions("LMLMLLRM");
+        assertEquals(8, lmlmllrm.size());
+    }
 
     @Test
-    void parseInstructions() {
+    @DisplayName("returns equal number of as List of InstructionEnum, when supplied valid number of arguments")
+    void testValidInstructions() {
+        List<InstructionEnum> parseInstructions = instructionParser.parseInstructions("LMLMR");
+        assertAll("check each instruction parsed Successfully",
+                ()-> assertEquals(InstructionEnum.L,parseInstructions.get(0)),
+                ()-> assertEquals(InstructionEnum.M,parseInstructions.get(1)),
+                ()-> assertEquals(InstructionEnum.L,parseInstructions.get(2)),
+                ()-> assertEquals(InstructionEnum.M,parseInstructions.get(3)),
+                ()-> assertEquals(InstructionEnum.R,parseInstructions.get(4))
+                );
     }
 }
