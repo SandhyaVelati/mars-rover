fe17eef1d7954d3d807f30372b9dc8be82903682
sandhya <svuksept2020@gmail.com>
2024-11-15 11:11:09 +0000

    refactor: refactor equals method in position

--- src/main/java/org/example/model/Position.java
+++ src/main/java/org/example/model/Position.java
@@ -45,10 +45,17 @@ public class Position {
                 '}';
     }
 
-    public boolean equals(Position inComingPosition) {
-        if (this == inComingPosition) return true;
-        if (inComingPosition == null || getClass() != inComingPosition.getClass()) return false;
-        return x == inComingPosition.x && y == inComingPosition.y && directionFacing == inComingPosition.directionFacing;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
 }
