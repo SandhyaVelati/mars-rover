eaed1fd1942a2a500efbe13815be38aa8a0b3d39
sandhya <svuksept2020@gmail.com>
2024-11-15 13:01:27 +0000

    feat: implement delegation of duties to Rover and plateau

--- src/main/java/org/example/model/MissionController.java
+++ src/main/java/org/example/model/MissionController.java
@@ -8,15 +8,17 @@ public class MissionController {
     public MissionController(int maxX, int maxY) {
         this.plateau = Plateau.getInstance(maxX, maxY);
     }
-    
     public void deployRoverOnPlateau(Rover rover){
         plateau.addRover(rover);
     }
-    
     public void driveRoverOnPlateau(Rover rover, List<InstructionEnum> instructions,Plateau plateau){
         rover.move(instructions, plateau);
     }
-
-
+    public void recallRover(Rover rover) {
+        plateau.removeRoverFromPlateau(rover);
+    }
+    public void listRovers() {
+        plateau.getRovers().forEach(rover -> System.out.println(rover.getRoverId() + " at " + rover.getPosition()));
+    }
 
 }
